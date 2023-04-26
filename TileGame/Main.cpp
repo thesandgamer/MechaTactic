4dc497930bfdd33cadb6f532c12c777cb005cf22
anouri <ultraadri@free.fr>
2023-04-26 12:27:02 +0200

    Comments

--- TileGame/Main.cpp
+++ TileGame/Main.cpp
@@ -9,12 +9,18 @@
 #include "Game.h"
 
 //++ToDo: Faire en sorte de passer le jeu en 3d:
+// Enlever le contrôle au joueur quand une unité s'active
 // Camera 3d: controllable(pour l'instant utiliser camear free)
-// Load de mesh pour les mechas, si le mesh est pas trouvé draw un debug cube
+// Dessin du chemin que le mecha va emprinter: 
+    // Indicateur du mecha selectionné
+    // Indicateur de la case sur laquelle est la souris(mettre ça dans curseur)
+    // Dessiner un chemin de cases en cases sur lequels va aller le mécha
+// Ui qui montre notre team et quand on clique dessus ça les séléctionne et focus la cam dessus
+
 // En plus:
-// Faire une map plus grande avec de la hauteur(map générée procéduralement)
 // Faire en sorte que les mechas attatquent
 // Faire une IA pour les mechas ennemis
+// Faire une map plus grande avec de la hauteur(map générée procéduralement), et avec un A* pas en grille
 
 
 using namespace std;
